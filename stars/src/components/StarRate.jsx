dad4b0ca573c71aa87e33bd42d88574f5e95985c
shehabMm <shehab.mm@gmail.com>
2023-11-04 06:45:26 +0200

    yes

--- stars/src/components/StarRate.jsx
+++ stars/src/components/StarRate.jsx
@@ -1,115 +1,61 @@
-
-import { FaStar } from "react-icons/fa";
-import "./star.css";
-import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from 'react-redux'
-import { getData } from '../Redux/imgSlice'
-import axios from 'axios'
+import { useState } from "react";
 
 const StarRate = () => {
-  const [list] = useState(["red", "green", "orange", "blue"]);
   const [text, setText] = useState("");
-  const { imgs } = useSelector((state) => state.imgSlice)
-  const [contain, setContain] = useState([]);
-  const dispatch = useDispatch()
-const sorted = contain.map((item)=> item.first_name ).sort((a,b)=>a.localeCompare(b))
-console.log(sorted)
+  const [array, setarray] = useState(["ali", "mona", "zaki"]);
 
+  console.log(array);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!array.includes(text)){
 
-  const getSata = async () => {
+      setarray([...array,text])
 
-    try {
-      const data = await axios.get('https://reqres.in/api/users')
-      setContain(data.data.data)
-    } catch (error) {
-      console.log(error.message);
     }
 
+    setText("")
 
-  }
+  };
 
-
-  useEffect(() => {
-    dispatch(getData())
-    getSata()
-
-  }, [dispatch])
-
-
-  let searchResault = list.filter((item) =>
-    item.toLowerCase().includes(text.toLowerCase())
-  );
-
-  const [rating, setRating] = useState(null);
   return (
-    <div className="main">
-      <div>
+    <ul>
+      <form onSubmit={handleSubmit}>
+        <button>Add Task</button>
+
         <input
+          id="text"
           type="text"
-          placeholder="Search now"
+          value={text}
           onChange={(e) => {
             setText(e.target.value);
           }}
         />
+      </form>
 
-        <ul>
-          {searchResault.map((item, index) => {
-            return (
-              <li
-                style={{ textDecoration: "none", listStyle: "none" }}
-                key={index}
-              >
-                {item}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
 
-      {[...Array(5)].map((item, index) => {
-        const currentRate = index + 1;
+      <div style={{display:"flex", }} >{array.map((item) => {
         return (
-          <div className="main" key={index}>
-            <label>
-              <input
-                name={rating}
-                style={{ display: "none" }}
-                onClick={() => {
-                  setRating(currentRate);
-                }}
-                type="radio"
-                value={currentRate}
-              />
-              <FaStar
-                size={30}
-                color={currentRate <= rating ? "orange" : "grey"}
-                className="star"
-              />
-            </label>
-          </div>
-        );
+        <ul key={Math.random()}>
+        <li  style={{textDecoration:"none", listStyle:"none"}}>{item}</li>
+        
+        <button onClick={()=>{
+console.log(item)
+
+let ss = array.filter((j)=>j!==item)
+console.log(ss);
+setarray(ss)
+
+        }}>del</button>
+        </ul>
+        )
+
       })}
-
-
-      <div>
-        {sorted.map((item) => {
-
-          return (
-            <div key={Math.random()}>
-
-              <h2 >{item}</h2>
-
-
-            </div>
-          )
-
-
-        })}
-
-
-
+      
+      
       </div>
-    </div>
+
+
+    </ul>
   );
 };
 
