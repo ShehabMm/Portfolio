b1ed414b92c459e53c8aa0b2e40e4435130fe0df
shehabMm <shehab.mm@gmail.com>
2023-11-10 08:58:11 +0200

    padding 0

--- stars/src/App.jsx
+++ stars/src/App.jsx
@@ -2,19 +2,25 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import MainContent from "./components/mainContent/MainContent";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <>
-    <Header/>
-
-    <BrowserRouter>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-    </>
+    <div className="container">
+      <Header />
+      <Hero />
+      <MainContent />
+      <Contact />
+      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ stars/src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div>
+      footer
+    </div>
+  );
+}
+
+export default Footer;
