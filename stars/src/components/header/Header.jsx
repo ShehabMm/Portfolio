dcbbbb254efa69ab0d3177240783a3a7f65c338a
shehabMm <shehab.mm@gmail.com>
2023-11-11 09:41:27 +0200

    icomoon

--- stars/src/components/header/Header.jsx
+++ stars/src/components/header/Header.jsx
@@ -1,33 +1,75 @@
-import './Header.css'
+import { useState } from "react";
+import "./Header.css";
 const Header = () => {
+
+const [showModel, setshowModel] = useState(false);
+
   return (
     <header>
-            <button className='show'>
-Show
-      </button>
+      <button className="show"  onClick={()=>{
 
-      <div/>
+setshowModel(!showModel)
+
+      }}>Show</button>
+
+      <div />
 
       <nav>
-
-<ul>
-
-<li><a href="">about</a> </li>
-<li><a href="">Articles</a> </li>
-<li> <a href="">Projects</a></li>
-<li><a href="">Speaking</a> </li>
-<li> <a href="">Contact</a></li>
-
-</ul>
+        <ul >
+          <li>
+            <a href="">about</a>
+          </li>
+          <li>
+            <a href="">Articles</a>
+          </li>
+          <li>
+          
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">Speaking</a>
+          </li>
+          <li>
+      
+            <a href="">Contact</a>
+          </li>
+        </ul>
       </nav>
+      <button>light</button>
+
+{showModel &&               (        <div className="popUp">
+<button onClick={()=>{
+
+setshowModel(false)
 
 
+}}>X</button>
+<ul style={{flexDirection:'column'}}>
+    <li>
+      <a href="">about</a>
+    </li>
+    <li>
+      <a href="">Articles</a>
+    </li>
+    <li>
+    
+      <a href="">Projects</a>
+    </li>
+    <li>
+      <a href="">Speaking</a>
+    </li>
+    <li>
 
-      <button>
-light
-      </button>
+      <a href="">Contact</a>
+    </li>
+  </ul>
+
+
+</div>
+    )}
+
     </header>
   );
-}
+};
 
 export default Header;
