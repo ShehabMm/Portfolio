644d7b359046a958e60c01aa8d906fada3dcab74
shehabMm <shehab.mm@gmail.com>
2023-11-24 22:44:16 +0200

    style

--- stars/src/components/mainContent/MainContent.jsx
+++ stars/src/components/mainContent/MainContent.jsx
@@ -1,44 +1,70 @@
-import './MainContent.css'
+import "./MainContent.css";
 const MainContent = () => {
   return (
-    <main className='main-content'>
-      <section className='left'>
-        <button className='active'>All Projects</button>
+    <main className="main-content">
+      <section className="left">
+        <button className="active">All Projects</button>
         <button>HTML & CSS</button>
         <button>Javascript</button>
         <button>Reactjs & MUI</button>
         <button>Node &Express</button>
-        </section>
+      </section>
 
-        <section className='right'>
-      <article>
+      <section className="right">
 
-
-<img src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1676312049/samples/ecommerce/leather-bag-gray.jpg" alt="" />
-<div className='box'>
-<h1>Landing Page2</h1>
-<p>This is the bigges project using mongo db and reactjs html css jsva script </p>
-
-</div>
-<div className='links-social'>
-  <div className='inner-parts'>   
-<div className='icon-link'></div>
-<div className='icon-github'></div>
+{ ["aa", "bb", "cc"].map((item)=>{
 
 
+return(
+
+
+  <article  key={item}>
+  <img
+    src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1676312049/samples/ecommerce/leather-bag-gray.jpg"
+    alt=""
+  />
+  <div className="box">
+    <h1>Landing Page2</h1>
+    <p>
+      This is the bigges project using mongo db and reactjs html css
+      jsva script{" "}
+    </p>
   </div>
-<div className='icon-arrow-right2'></div>
+  <div className="links-social">
+    <div className="inner-parts">
+      <div className="icon-link"></div>
+      <div className="icon-github"></div>
+    </div>
+
+    <div className="arrow20">
+      <a className="linka" href="">
+        more
+      </a>
+
+      <span className="icon-arrow-right2"></span>
+    </div>
+  </div>
+</article>
 
 
-</div>
+
+)
 
 
 
-      </article>
-        </section>
+})        }
+
+      </section>
+
+
+
+
+
+
+
 
     </main>
   );
-}
+};
 
 export default MainContent;
