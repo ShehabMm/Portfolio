d6cfba000ac230c617d96443f830b1ca6a132dd6
shehabMm <shehab.mm@gmail.com>
2023-11-24 19:36:32 +0200

    box

--- stars/src/components/mainContent/MainContent.jsx
+++ stars/src/components/mainContent/MainContent.jsx
@@ -3,7 +3,7 @@ const MainContent = () => {
   return (
     <main className='main-content'>
       <section className='left'>
-        <button>All Projects</button>
+        <button className='active'>All Projects</button>
         <button>HTML & CSS</button>
         <button>Javascript</button>
         <button>Reactjs & MUI</button>
@@ -11,7 +11,30 @@ const MainContent = () => {
         </section>
 
         <section className='right'>
-          r
+      <article>
+
+
+<img src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1676312049/samples/ecommerce/leather-bag-gray.jpg" alt="" />
+<div className='box'>
+<h1>Landing Page2</h1>
+<p>This is the bigges project using mongo db and reactjs html css jsva script </p>
+
+</div>
+<div className='links-social'>
+  <div className='inner-parts'>   
+<div className='icon-link'></div>
+<div className='icon-github'></div>
+
+
+  </div>
+<div className='icon-arrow-right2'></div>
+
+
+</div>
+
+
+
+      </article>
         </section>
 
     </main>
