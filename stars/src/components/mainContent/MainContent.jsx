9c4388cc653912d0212223c193a43709ab0ab534
shehabMm <shehab.mm@gmail.com>
2023-11-27 16:44:34 +0200

    toggle

--- stars/src/components/mainContent/MainContent.jsx
+++ stars/src/components/mainContent/MainContent.jsx
@@ -1,14 +1,51 @@
 import "./MainContent.css";
+import {useState} from 'react';
+
+
+
 const MainContent = () => {
+const [alter, setAlter]=useState("all")
+
   return (
     <main className="main-content">
       <section className="left">
         <button className="active">All Projects</button>
-        <button>HTML & CSS</button>
-        <button>Javascript</button>
-        <button>Reactjs & MUI</button>
-        <button>Node &Express</button>
+
+
+        <button onClick={()=>{
+
+setAlter('CSS')
+
+        }} className={alter==="CSS"? "active":null}  >HTML & CSS</button>
+
+
+        <button onClick={()=>{
+
+setAlter('Javascript')
+
+        }} className={alter==='Javascript'? "active":null} >Javascript</button>
+
+
+
+
+   <button onClick={()=>{
+
+ setAlter('Reactjs')
+
+         }} className={alter==='Reactjs'? "active":null} >Reactjs & MUI</button>
+        <button onClick={()=>{
+
+setAlter('Node')
+
+       }} className={alter==='Node'? "active":null} >Node &Express</button>
+
+
+
+
+
+
       </section>
+
 
       <section className="right">
 
