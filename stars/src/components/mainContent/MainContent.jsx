b4f4f6f9691d5de7fe1ae8f34e8038d994db53ab
shehabMm <shehab.mm@gmail.com>
2023-11-30 20:34:19 +0200

    logic

--- stars/src/components/mainContent/MainContent.jsx
+++ stars/src/components/mainContent/MainContent.jsx
@@ -1,105 +1,114 @@
 import "./MainContent.css";
-import {useState} from 'react';
+import { useState } from "react";
 
 
+const myProject = [{projectTitle:"React project", category:'react', imgPath:"a"}, 
+{projectTitle:"Css project", category:'css', imgPath:"b"},
+{projectTitle:"Mongo project", category:'mongo', imgPath:"c"}
+]
 
 const MainContent = () => {
-const [alter, setAlter]=useState("all")
+  const [alter, setAlter] = useState("all");
+
+  const [arr, setArr] = useState(myProject)
 
   return (
     <main className="main-content">
       <section className="left">
-        <button className="active">All Projects</button>
-
-
         <button onClick={()=>{
-
-setAlter('CSS')
-
-        }} className={alter==="CSS"? "active":null}  >HTML & CSS</button>
+setAlter('all')
 
 
-        <button onClick={()=>{
-
-setAlter('Javascript')
-
-        }} className={alter==='Javascript'? "active":null} >Javascript</button>
+        }}   className={alter==='all'?"active":null}>All Projects</button>
 
 
 
-
-   <button onClick={()=>{
-
- setAlter('Reactjs')
-
-         }} className={alter==='Reactjs'? "active":null} >Reactjs & MUI</button>
-        <button onClick={()=>{
-
-setAlter('Node')
-
-       }} className={alter==='Node'? "active":null} >Node &Express</button>
+        <button
+          onClick={() => {
+            setAlter("CSS");
 
 
+        const filtered = myProject.filter((item)=>{
+
+return item.category ==="css"
+
+            })
+
+            setArr(filtered)
+
+          }}
+          className={alter === "CSS" ? "active" : null}
+        >
+          HTML & CSS
+        </button>
+        <button
+          onClick={() => {
+            setAlter("Javascript");
+          }}
+          className={alter === "Javascript" ? "active" : null}
+        >
+          Javascript
+        </button>
+        <button
+          onClick={() => {
+            setAlter("Reactjs");
+
+const filter = myProject.filter((item)=>{
+
+return item.category ==="react"
+
+})
 
 
-
-
+setArr(filter)
+          }}
+          className={alter === "Reactjs" ? "active" : null}
+        >
+          Reactjs & MUI
+        </button>
+        <button
+          onClick={() => {
+            setAlter("Node");
+          }}
+          className={alter === "Node" ? "active" : null}
+        >
+          Node &Express
+        </button>
       </section>
-
-
       <section className="right">
-
-{ ["aa", "bb", "cc", "dd","ee", "ll"].map((item)=>{
-
-
-return(
-
-
-  <article  key={item}>
-  <img
-    src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1676312049/samples/ecommerce/leather-bag-gray.jpg"
-    alt=""
-  />
-  <div className="box">
-    <h1>Landing Page2</h1>
-    <p>
-      This is the bigges project using mongo db and reactjs html css
-      jsva script{" "}
-    </p>
-  </div>
-  <div className="links-social">
-    <div className="inner-parts">
-      <div className="icon-link"></div>
-      <div className="icon-github"></div>
-    </div>
-
-    <div className="arrow20">
-      <a className="linka" href="">
-        more
-      </a>
-
-      <span className="icon-arrow-right2"></span>
-    </div>
-  </div>
-</article>
+        {arr.map((item) => {
 
 
 
-)
-
-
-
-})        }
-
+          return (
+            <article key={item.imgPath}>
+              <img
+                src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1676312049/samples/ecommerce/leather-bag-gray.jpg"
+                alt=""
+              />
+              <div className="box">
+                <h1>{item.projectTitle}</h1>
+                <p>
+                  This is the bigges project using mongo db and reactjs html css
+                  jsva script{" "}
+                </p>
+              </div>
+              <div className="links-social">
+                <div className="inner-parts">
+                  <div className="icon-link"></div>
+                  <div className="icon-github"></div>
+                </div>
+                <div className="arrow20">
+                  <a className="linka" href="">
+                    more
+                  </a>
+                  <span className="icon-arrow-right2"></span>
+                </div>
+              </div>
+            </article>
+          );
+        })}
       </section>
-
-
-
-
-
-
-
-
     </main>
   );
 };
